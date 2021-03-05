d10885a4dae8394a1fd7dcbdcabdbbed69f37f87
AndresTam <19030296@itcelaya.edu.mx>
2021-03-04 18:20:48 -0600

    Clase 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,47 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
+
+    private HBox hBox;
+    private VBox vBox;
+    private Button btn1, btn2, btn3;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        CrearUI();
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(vBox, 300, 150));
         primaryStage.show();
+    }
+
+    private void CrearUI(){
+        btn1 = new Button("Boton 1");
+        btn2 = new Button("Boton 2");
+        btn3 = new Button("Boton 3");
+
+        hBox = new HBox();
+        vBox = new VBox();
+        hBox.getChildren().addAll(btn1, btn2, btn3);
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(20,20,20,20));
+
+        vBox.getChildren().addAll(btn1, btn2, btn3);
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(20));
     }
 
 
