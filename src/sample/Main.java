09564b9d2a076f057e2f96ffdc62bbe5c8167afd
AndresTam <19030296@itcelaya.edu.mx>
2021-03-25 13:43:37 -0600

    Clase 11

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,50 +1,73 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.awt.*;
+import sample.views.Calculadora;
+import sample.views.Encriptador;
+import sample.views.Rompecabezas;
 
 public class Main extends Application {
-
-    private HBox hBox;
     private VBox vBox;
-    private Button btn1, btn2, btn3;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1, menCompetencia2, menCerrar;
+    private MenuItem mitCalcu, mitRompecabezas, mitEncriptar,mitSalir;
+    private Scene escena;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        CrearUI();
+        CrearMenu();
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(vBox, 300, 150));
+        primaryStage.setTitle("Proyecto de Clase TAP 2021");
+        primaryStage.setScene(escena);
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
-    private void CrearUI(){
-        btn1 = new Button("Boton 1");
-        btn2 = new Button("Boton 2");
-        btn3 = new Button("Boton 3");
-
-        hBox = new HBox();
+    private void CrearMenu(){
         vBox = new VBox();
-        hBox.getChildren().addAll(btn1, btn2, btn3);
-        hBox.setSpacing(10);
-        hBox.setPadding(new Insets(20,20,20,20));
+        mnbPrincipal    = new MenuBar();
+        menCompetencia1 = new Menu("Competencia 1");
+        menCompetencia2 = new Menu("Competencia 2");
+        menCerrar       = new Menu("Cerrar");
+        mnbPrincipal.getMenus().addAll(menCompetencia1,menCompetencia2,menCerrar);
 
-        vBox.getChildren().addAll(btn1, btn2, btn3);
-        vBox.setSpacing(10);
-        vBox.setPadding(new Insets(20));
+        mitCalcu = new MenuItem("Calculadora");
+        mitCalcu.setOnAction(event -> opcionesMenu(1));
+        mitRompecabezas = new MenuItem("Rompecabezas");
+        mitRompecabezas.setOnAction(event -> opcionesMenu(2));
+        mitEncriptar = new MenuItem("Encriptador");
+        mitEncriptar.setOnAction(event -> opcionesMenu(3));
+        menCompetencia1.getItems().addAll(mitCalcu, mitRompecabezas, mitEncriptar);
+
+        mitSalir = new MenuItem("Salir");
+        mitSalir.setOnAction(event -> {System.exit(0);});
+        menCerrar.getItems().add(mitSalir);
+        vBox.getChildren().add(mnbPrincipal);
+
+        escena = new Scene(vBox, 300, 70);
     }
 
+    private void opcionesMenu(int opc){
+        switch (opc){
+            case 1: new Calculadora(); break;
+            case 2: new Rompecabezas().show(); break;
+            case 3: new Encriptador().show(); break;
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
